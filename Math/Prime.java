298cb20bf8d3688ce4862aa8e0d131c6106371ea
kavya <athmakurukavya8@gmail.com>
2025-07-25 17:26:28 +0530

    Adding prime number problem

--- Math/Prime.java
+++ Math/Prime.java
@@ -2,7 +2,7 @@ package Math;
 
 public class Prime {
     public static void main(String[] args) {
-        int n =10;
+        int n =2;
         boolean ans = false;
         if(n<=1){
             System.out.println(n+" Is not prime");
