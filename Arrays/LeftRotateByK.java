ee7cbba1f0e3f5f4e8f676e749ceb7d8185c65b1
kavya <athmakurukavya8@gmail.com>
2025-08-13 16:41:26 +0530

    Left rotate the array bt k time

---
+++ Arrays/LeftRotateByK.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class LeftRotateByK {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int k = 3;
+        k = k % (arr.length);
+        for(int i=0; i<=k; i++){  
+            rotate(arr);
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static int[] rotate(int[] arr){
+        int temp = arr[0];
+        for (int i = 0; i <arr.length-1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length-1] = temp;
+        return arr;
+    }
+}
