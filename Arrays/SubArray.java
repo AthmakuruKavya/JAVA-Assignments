7576c3ea95994b879b845c5399fdfa213edf4cf3
kavya <athmakurukavya8@gmail.com>
2025-08-21 22:06:50 +0530

    Finding sub array for given sum

---
+++ Arrays/SubArray.java
@@ -0,0 +1,17 @@
+public class SubArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        int k = 5;
+        int count = 0;
+        for(int i=0; i<arr.length; i++){
+            int sum = 0;
+            for(int j=i; j<arr.length; j++){
+                sum = sum+arr[j];
+                if(sum == k){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
