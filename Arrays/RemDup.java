a187bcb3664eaaaaf71174a1f04f05fc918c099d
kavya <athmakurukavya8@gmail.com>
2025-08-21 22:05:50 +0530

    Removing deplicates from the given array

--- Arrays/RemDup.java
+++ Arrays/RemDup.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class RemDup {
     public static void main(String[] args) {
-        int[] arr = {10,20,20,30,30,30};
+        int[] arr ={2,3,4,3,3,2} ;         //{10,20,20,30,30,30}
         ArrayList<Integer> al = new ArrayList<>();
         for(int i=0; i<arr.length; i++){
             int j = i-1;
