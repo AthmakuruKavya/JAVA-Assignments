4e7ea63df66bd48763e87646d4a9ab030ea02cfe
kavya <athmakurukavya8@gmail.com>
2025-09-10 12:15:31 +0530

    fibonacci program using recursion

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    static int fib(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int val = fib(n);
+        System.out.println(val);
+    }
+}
